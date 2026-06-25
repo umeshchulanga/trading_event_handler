c7504f84e3d113a4337c6e8c8caba7d114836c74
umeshchulanga <161038217+umeshchulanga@users.noreply.github.com>
2026-06-25 15:07:23 +0530

    Add files via upload

---
+++ data-lists.js
@@ -0,0 +1,6 @@
+
+window.TRADING_EVENT_LISTS = {
+  tradingTypes: ["Spot", "Future", "Convert", "State", "Earn", "Tradfi", "Watch", "EFT","Depin","Check-in"],
+  eventCategories: ["Gate.io", "Mexe", "Airdrop", "Other"],
+  checkingTypes: ["Daily", "Weekly", "Monthly", "Once"]
+};
