9e730e2fc0459f977f97f33c0de3ff8f9ffbfc93
umeshchulanga <161038217+umeshchulanga@users.noreply.github.com>
2026-06-25 15:06:09 +0530

    Delete data-lists.js

--- data-lists.js
+++
@@ -1,5 +0,0 @@
-window.TRADING_EVENT_LISTS = {
-  tradingTypes: ["Spot", "Future", "Convert", "State", "Earn", "Tradfi", "Watch","EFT","de-in","Depin"],
-  eventCategories: ["Gate.io", "Mexe", "Airdrop", "Other"],
-  checkingTypes: ["Daily", "Weekly", "Monthly", "Once"]
-};
