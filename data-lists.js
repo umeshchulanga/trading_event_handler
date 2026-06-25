79983651c342ef73003f286351e942d2cfb58ba3
umeshchulanga <161038217+umeshchulanga@users.noreply.github.com>
2026-06-25 14:56:24 +0530

    Update data-lists.js

--- data-lists.js
+++ data-lists.js
@@ -1,5 +1,5 @@
 window.TRADING_EVENT_LISTS = {
-  tradingTypes: ["Spot", "Future", "Convert", "State", "Earn", "Tradfi", "Watch","EFT"],
+  tradingTypes: ["Spot", "Future", "Convert", "State", "Earn", "Tradfi", "Watch","EFT","Check-in"],
   eventCategories: ["Gate.io", "Mexe", "Airdrop", "Other"],
   checkingTypes: ["Daily", "Weekly", "Monthly", "Once"]
 };
